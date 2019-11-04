2808569875386d168ec32ca19d60e52c3b31e1f8
Christian Gomez <jiminiiik@gmail.com>
2019-11-04 17:07:43 -0500

    added schemas and root resolvers

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Ready to go!');
-});
+app.use('/api', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            digimonList: [String!]!
+        }
+
+        type RootMutation {
+            createDigimon(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        digimonList: () => {
+            return ['Agumon', 'Betamon', 'ShineGreymon']
+        },
+        createDigimon: (args) => {
+            const digimonName = args.name;
+            return digimonName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
